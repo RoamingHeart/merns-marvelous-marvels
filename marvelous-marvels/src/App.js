084c70b6e93569f06b3d098341a91415e97bb80c
RoamingHeart <aggoden@gmail.com>
2023-09-09 17:58:18 -0400

    finished based code, needs debugging

--- marvelous-marvels/src/App.js
+++ marvelous-marvels/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+//routes
+import Home from './routes/Home';
+import Login from './routes/Login'
+import HeroDetails from './routes/HeroDetails'
+
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path={'/'} element={<Home />}/>
+        <Route path={'login'} element={<Login />}/>
+        <Route path={'/:id'} element={<HeroDetails />}/>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
