845fa97ab1fd8fb15532e01c494d853dcae7b6df
RoamingHeart <aggoden@gmail.com>
2023-09-11 21:15:22 -0400

    fixed some minor codes to fit the api schema

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -2,30 +2,82 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedHeroes` array in User.js
 const heroSchema = new Schema({
-  authors: [
-    {
-      type: String,
-    },
-  ],
-  description: {
-    type: String,
-    required: true,
+  code: int,
+  status: string,
+  copyright: string,
+  attributionText: string,
+  attributionHTML: string,
+  data: {
+    offset: int,
+    limit: int,
+    total: int,
+    count: int,
+    results: [
+      {
+        id: int,
+        name: string,
+        description: string,
+        modified: Date,
+        resourceURI: string,
+        urls: [
+          {
+            type: string,
+            url: string
+          }
+        ],
+        thumbnail: {
+          path: string,
+          extension: string
+        },
+        comics: {
+          available: int,
+          returned: int,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        },
+        stories: {
+          available: int,
+          returned: int,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string,
+              type: string
+            }
+          ]
+        },
+        events: {
+          available: int,
+          returned: int,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        },
+        series: {
+          available: int,
+          returned: int,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        }
+      }
+    ]
   },
-  // saved hero id from Marvel API
-  heroId: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
+  etag: string
 });
 
 module.exports = heroSchema;
