b36fa7760c6168f87b7001b44530120ef38b46dd
RoamingHeart <aggoden@gmail.com>
2023-09-18 19:56:08 -0400

    added a button

--- client/src/components/HeroDetails.jsx
+++ client/src/components/HeroDetails.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Button } from 'react-bootstrap'
 
 import { fetchHero } from "../libs/utils";
+import Auth from '../libs/auth'
+import { saveHero  } from '../libs/userForms';
+import { saveHeroIds, getSavedHeroIds } from '../libs/localStorage';
 
 export default function HeroDetails() {
   let { id } = useParams();
 
   const [hero, setHero] = useState();
+  const [savedHeroIds, setSavedHeroIds] = useState(getSavedHeroIds())
+  
+  useEffect(() => {
+    return () => saveHeroIds(savedHeroIds);
+  });
 
   let name;
   let description;
@@ -32,6 +41,31 @@ export default function HeroDetails() {
 
   if (!hero) return;
 
+  const handleSaveHero = async (heroId) => {
+    // find the book in `searchedBooks` state by the matching id
+    const heroToSave = hero.find((hero) => hero.heroId === heroId);
+
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const response = await saveHero(heroToSave, token);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      // if book successfully saves to user's account, save book id to state
+      setSavedHeroIds([...savedHeroIds, heroToSave.heroId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="container large">
       <div className="hero__details-container">
@@ -39,6 +73,16 @@ export default function HeroDetails() {
         <div className="hero__details">
           <h4>Name</h4>
           <p>{name}</p>
+          {Auth.loggedIn() && (
+            <Button
+              disabled={savedHeroIds?.some((savedHeroId) => savedHeroId === hero.heroId)}
+              className='btn-block btn-info'
+              onClick={() => handleSaveHero(hero.heroId)}>
+                {savedHeroIds?.some((savedHeroId) => savedHeroId === hero.heroId)
+                  ? 'This hero has already been saved!'
+                  : 'Favorite this Hero!'}
+            </Button>
+          )}
           {description ? (
             <>
               <h4>Description</h4>
